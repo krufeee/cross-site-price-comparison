538a2a3b50319deeb88138566748dc37122ebaa7
krufeee <krufeee@gmail.com>
2026-03-08 08:16:53 +0200

    counted products in technomarket

--- site_scrapers/technomarket/technomarket_store_scraper.py
+++ site_scrapers/technomarket/technomarket_store_scraper.py
@@ -3,6 +3,9 @@ import re
 import pandas as pd
 from playwright.sync_api import sync_playwright
 
+from site_scrapers.technomarket.parser import scrape_products
+
+
 def technomarket_store_scraper():
     try:
         with sync_playwright() as p:
@@ -41,38 +44,54 @@ def technomarket_store_scraper():
                             for sub_category_entry in all_subcategory_entries:
                                 sub_category_name = sub_category_entry.inner_text()
                                 sub_category_link = sub_category_entry.get_attribute('href')
-                                categories.append({'name' : sub_category_name, 'link' : sub_category_link})
+                                categories.append({'name' : sub_category_name, 'link' : base_url + sub_category_link})
                         else:
+                            addon = '/produkti'
                             category = category_locator.first
                             category_name = category.inner_text()
                             category_link = category.get_attribute('href')
-                            categories.append({'name' : category_name, 'link' : category_link})
+                            categories.append({'name' : category_name, 'link' : base_url + addon + category_link})
 
 
                 df = pd.DataFrame(categories)
                 df = df.drop_duplicates(subset=["link"])
-                df.to_csv(f"../../database/categories_technomarket.csv", index=False, encoding="utf-8-sig")
+                df.to_csv(f'../../data/categories/categories_technomarket.csv', index=False, encoding='utf-8-sig')
 
                 print('Categories extracted')
 
                 df = pd.read_csv('../../data/categories/categories_technomarket.csv')
-                categories_dict_from_csv = df.to_dict(orient="records")
-
+                categories_dict_from_csv = df.to_dict(orient='records')
+                products_count = 0
                 for category in categories_dict_from_csv:
-                    current_link = base_url + category['link']
+                    current_link = category['link']
                     current_name = category['name']
-                    print(f'Extracting category - {current_name} ...')
+                    print(f'Extracting category - {current_name} - {current_link}')
                     page.goto(current_link)
-                    pagination = page.locator('.pages > a')
-                    if pagination.count() > 0:
-                        next_page_button = pagination.last
-                        next_button = next_page_button.get_attribute('.class')
-                        print(next_button)
-                    else:
-                        print('Only one page here')
+
+                    page_num = 1
+                    while True:
+
+                        url = f"{current_link}?page={page_num}"
+                        page.goto(url)
+
+                        products = page.locator('tm-product-item')
+
+                        if products.count() == 0:
+                            break
+
+                        products_count += scrape_products(products)
+
+                        page_num += 1
+                    # pagination = page.locator('.pages > a')
+                    # if pagination.count() > 0:
+                    #     next_page_button = pagination.last
+                    #     next_button = next_page_button.get_attribute('.class')
+                    #     print(next_button)
+                    # else:
+                    #     print('Only one page here')
 
 
-
+                print(products_count)
 
 
 
